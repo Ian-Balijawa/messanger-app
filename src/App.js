2c62238fd1e8ae655e5594029cde5a43d8894934
BannyT <tumusiimebarnabus@gmail.com>
2021-06-24 12:16:25 +0300

    styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-       <h1>This is our messaging app home page component</h1>
+       <div className="main-body">
+          
+       </div>
+       {/* <h1>This is our messaging app home page component</h1> */}
+
     </div>
   );
 }
