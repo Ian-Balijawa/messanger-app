e3c94a1a2e28948cc64f969369f517c0f4576a30
BannyT <tumusiimebarnabus@gmail.com>
2021-06-24 13:36:35 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
        <div className="main-body">
+         <Sidebar/>
           
        </div>
        {/* <h1>This is our messaging app home page component</h1> */}
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Sidebar.css"
+
+function Sidebar() {
+  return (
+    <div className="side-bar">
+        <h3>Here is my sidebar</h3>
+    </div>
+  )
+}
+
+export default Sidebar
