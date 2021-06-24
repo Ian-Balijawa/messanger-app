fa01eb0100f26c09d907e87eae04cca6ff93b60e
BannyT <tumusiimebarnabus@gmail.com>
2021-06-24 14:22:09 +0300

    new code

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import "./Sidebar.css"
-import Avatar from '@material-ui/core/Avatar';
+import {Avatar, IconButton} from "@material-ui/core"
+import { DonutLarge } from '@material-ui/icons'
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+// import SidebarChat from './SidebarChat'
 
 function Sidebar() {
   return (
     <div className="side-bar">
            <div className="sidebar-header">
                <Avatar/>
+               <div className="header-right">
+                  <IconButton>
+                  <DonutLarge/>
+                  </IconButton>
+                  <IconButton>
+                  <ChatIcon/>
+                  </IconButton>
+                  <IconButton>
+                  <MoreVertIcon/>
+                 </IconButton>
+         </div>
            </div>
     </div>
   )
