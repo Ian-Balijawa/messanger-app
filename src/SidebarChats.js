9b7c6e93136be73511c036e12803554e48363401
BannyT <tumusiimebarnabus@gmail.com>
2021-06-25 14:01:38 +0300

    new code

--- src/SidebarChats.js
+++ src/SidebarChats.js
@@ -3,8 +3,12 @@ import "./SidebarChats.css"
 
 function SidebarChats() {
   return (
-    <div>
-      <h4>this is may sidebar message</h4>
+    <div className="chats">
+      <Avatar src='https://avatars.dicebear.com/api/human/999.svg'/>
+     <div className="chat-info">
+       <h2>Mesage Title</h2>
+       <p>Hello guys hows the build</p>
+     </div>
     </div>
   )
 }
