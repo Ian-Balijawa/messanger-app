dddbd10800f38786d95b68167eaf0a279d64da94
BannyT <tumusiimebarnabus@gmail.com>
2021-06-28 11:09:40 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Sidebar';
-
+import MainChat from './MainChat'
 function App() {
   return (
     <div className="app">
        <div className="main-body">
          <Sidebar/>
+         <MainChat/>
           
        </div>
-       {/* <h1>This is our messaging app home page component</h1> */}
 
     </div>
   );
---
+++ src/MainChat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./MainChat.css"
+
+function MainChat() {
+  return (
+    <div>
+        <h5>this is our messaging area</h5>
+    </div>
+  )
+}
+
+export default MainChat
