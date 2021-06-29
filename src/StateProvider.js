e6b199aab13105e8bc3f623f2d598dce469f4eae
BannyT <tumusiimebarnabus@gmail.com>
2021-06-29 14:12:26 +0300

    state provider

---
+++ src/StateProvider.js
@@ -0,0 +1,12 @@
+import React, { createContext ,useContext,useReducer} from "react";
+
+// set up data layer to track the basket
+export const StateContext=createContext();
+
+//Build  a provider
+export const StateProvider  = ({reducer,initialState,children}) => (
+  <StateContext.Provider value={useReducer(reducer,initialState)}>
+    {children}
+  </StateContext.Provider>  
+);
+export const useStateValue= ()=>useContext(StateContext)
