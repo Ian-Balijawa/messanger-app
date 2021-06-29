814161264add039866b8f0dd2d018f7e6f06ce82
BannyT <tumusiimebarnabus@gmail.com>
2021-06-29 14:34:19 +0300

    reducer

--- src/Reducer.js
+++ src/Reducer.js
@@ -4,3 +4,18 @@ export const initialState={
     user: null,
 }
 
+// implement reducer to manage user state
+
+const reducer =( state,action)=>{
+    switch(action.type){ 
+        case  'SET_USER':
+        return {
+            ...state,
+            user:action.user,
+        } 
+        default:
+        return state; 
+    }
+}
+export default reducer
+
