392336efcca702efe02c88f5e857b876306f7336
BannyT <tumusiimebarnabus@gmail.com>
2021-06-30 14:45:40 +0300

    added  random avartar

--- src/SidebarChats.js
+++ src/SidebarChats.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import "./SidebarChats.css"
 import {Avatar, IconButton} from "@material-ui/core"
 
 function SidebarChats({channelname,id}) {
+   const[randomNumber,setRandom]=useState('')
+
+    //algo to generate random number
+    useEffect(()=>{
+        setRandom(Math.floor(Math.random()*10000))
+    },[])
+
   return (
     <div className="chats">
-      <Avatar src='https://avatars.dicebear.com/api/human/999.svg'/>
+      <Avatar src={`https://avatars.dicebear.com/api/human/${randomNumber}.svg`}/>
      <div className="chat-info">
        <h2>{channelname}</h2>
        <p>Hello guys hows the build</p>
