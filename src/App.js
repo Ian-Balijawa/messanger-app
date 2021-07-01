273531d582c06bb273946394a7602471423c0e95
BannyT <tumusiimebarnabus@gmail.com>
2021-07-01 13:13:22 +0300

    new data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import logo from './logo.svg';
-import React from "react";
+import React,{useEffect} from "react";
 import './App.css';
 import Sidebar from './Sidebar';
 import MainChat from './MainChat';
 import Login from "./Login"
-
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { useStateValue } from './StateProvider';
+import {auth} from "./firebase"
 function App() {
+  const[{user},dispatch]=useStateValue()
+
+   // functionality to manipulate our state
+      useEffect(()=>{
+        const unsubcribe = auth.onAuthStateChanged(authuser=>{
+          if(authuser){
+            dispatch({
+              type:'SET_USER',
+              user:authuser
+            })
+          }
+          else{
+            dispatch({
+              type:'SET_USER',
+              user:null
+            })
+          }
+        })     
+        return ()=>{unsubscribe()}
+      },[])
+
+
+
+
+
+
   return (
     <div className="app">
-       <div className="main-body">
-         <Router>
-           <Switch>
-               <Route exact path="/"> 
-               <Sidebar/>
-               <MainChat/>
-               </Route>
-               <Route exact path="/login">
-                 <Login/>
-               </Route>
-           </Switch>
-         </Router> 
-       </div>
+       {!user?(
+      <Login/>
+     ):(
+      <div className="main-body">
+      <Router>
+        <Switch>
+            <Route exact path="/"> 
+            <Sidebar/>
+            <MainChat/>
+            </Route>
+        </Switch>
+      </Router> 
+    </div>
+     )
+
+    }
+       
 
     </div>
   );
