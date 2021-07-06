117bb1ac5eb6ad43612a3c08e8fc759f7dba5a41
BannyT <tumusiimebarnabus@gmail.com>
2021-07-06 15:09:53 +0300

    update

--- src/MainChat.js
+++ src/MainChat.js
@@ -7,6 +7,7 @@ import SearchIcon from '@material-ui/icons/Search';
 import { DonutLarge, InsertEmoticon } from '@material-ui/icons'
 import {useParams} from "react-router-dom"
 import db from "./firebase"
+import {auth} from "./firebase"
 import {useStateValue} from './StateProvider'
 import firebase from "firebase";
 import {toast } from 'react-toastify';
@@ -41,7 +42,7 @@ const useStyles = makeStyles({
       const {roomId}=useParams()
       const [message,setMessage]=useState('')
       const [messages,setMessages]=useState([])
-      const [{user}]=useStateValue()
+      const [{user},dispatch]=useStateValue()
       const [channelName, setChannel]=useState('')
    
       
@@ -84,7 +85,15 @@ const useStyles = makeStyles({
                   }      
            },[roomId])
 
-          //  function to clear messages goes here
+          //  function to logout of app
+           const logOut=()=>{
+             auth.signOut()
+             dispatch({
+              type:'SET_USER',
+              user:null
+             })
+             this.handleClose();
+           }
 
   return (
     <div className="chat-field">
@@ -151,7 +160,7 @@ const useStyles = makeStyles({
               <AddIcon />
             </Avatar>
           </ListItemAvatar>
-          <ListItemText primary="Logout of your Acccount" />
+          <ListItemText onClick={logOut} primary="Logout of your Acccount" />
         </ListItem>
       </List>
     </Dialog>
