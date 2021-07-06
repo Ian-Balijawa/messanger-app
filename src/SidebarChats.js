637b724c1d5280941906cb548dd52916399d5393
BannyT <tumusiimebarnabus@gmail.com>
2021-07-06 13:05:11 +0300

    data update

--- src/SidebarChats.js
+++ src/SidebarChats.js
@@ -4,14 +4,25 @@ import {Avatar, IconButton} from "@material-ui/core"
 import { Link} from "react-router-dom";
 import Badge from '@material-ui/core/Badge';
 import MailIcon from '@material-ui/icons/Mail';
-
+import db from "./firebase"
 function SidebarChats({channelname,id}) {
    const[randomNumber,setRandom]=useState('')
+   const [messages,setMessages]=useState([])
 
     //algo to generate random number
     useEffect(()=>{
         setRandom(Math.floor(Math.random()*10000))
     },[])
+
+    //useEffect to render last message in this channel
+    useEffect(()=>{
+          if(id){
+            db.collection('Room').doc(id).collection('Messages').orderBy('timestamp','asc').onSnapshot(snap=>{
+              setMessages(snap.doc.map(doc=>doc.data()))
+            })
+          }
+    },[])
+    console.log(messages)
 
   return (
     <Link className="link" to={`/rooms/${id}`}>
